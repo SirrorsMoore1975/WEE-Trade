f17f12c1e429ed44da042f45c1a821d9ec134497
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2023-04-20 21:23:48 +0800

    polish up server to read post request /api/user

--- ccp7-wee-trade/src/components/button/Button.jsx
+++ ccp7-wee-trade/src/components/button/Button.jsx
@@ -1,14 +1,39 @@
-import React, {useState, Link} from 'react';
+import React, {useState, useEffect, Link} from 'react';
 
 
 
 
-export default function Button (){
+export default function Button ({btnClassName, value, htmlFor}){
+    // if you want to use a different className for the button for a different css style
+    const [useBtnClassName, setUseBtnName] = useState('');
+    const [useValueName, setValueName] = useState('');
+    useEffect(()=>{
+        handleBtnClassName();
+        handleValueName();
+    }, [])
+    const handleBtnClassName = ()=> {
+        if(btnClassName){
+            setUseBtnName(btnClassName);
 
+        } else {
+            setUseBtnName("btn")
+        }
+    }
+    const handleValueName = ()=> {
+        if(value){
+            setValueName(value);
 
+        } else {
+            setValueName("Submit")
+        }
+    }
+    // const wrapperFunc = () => {
+    //     handleBtnClassName();
+    //     handleValueName();
+    // }
     return (
         <>
-
+        <button className={useBtnClassName} htmlFor={htmlFor} type='submit' >{useValueName}</button>
         
         </>
     )
--- ccp7-wee-trade/src/pages/Home.jsx
+++ ccp7-wee-trade/src/pages/Home.jsx
@@ -4,7 +4,8 @@ import React, {useState} from 'react';
 import { Link } from 'react-router-dom'
 import Post from './../components/post/Post';
 import NavBar from './../components/bar/NavBar';
-import SideBar from './../components/bar/SideBar'
+import SideBar from './../components/bar/SideBar';
+import Button from './../components/button/Button';
 import './Home.css';
 import axios from 'axios'
 
@@ -25,6 +26,7 @@ function Home(){
             <div className='test'>
              <div className='row1'>
                 <NavBar />
+                <Button value="Hot Value Deal"></Button>
              </div>
              <div>
             <div className='row2'>
