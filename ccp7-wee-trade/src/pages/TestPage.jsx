0abeeab846a331a2eda35fca65de7ab6b88dade9
Chadwick <88133280+SirrorsMoore1975@users.noreply.github.com>
2023-04-25 17:28:15 +0800

    updating post page (#32)
    Co-authored-by: SirrorsMoore1975 <SirrorsMoore1975@github.com>

--- ccp7-wee-trade/src/pages/TestPage.jsx
+++ ccp7-wee-trade/src/pages/TestPage.jsx
@@ -14,12 +14,17 @@ function TestPage () {
     return (
         <div>
             <div>
-            <Post post_id="2345" seller={{id:"1466", name:"Dowdon"}} amount="5000" desc="Excellent GPU" hasSold={false} img_url="https://i.ibb.co/x38n99S/Space-Flame-nasa-1100.jpg" alt="anything"></Post>
+            <Post post_id="2345" seller={{id:"1466", name:"Dowdon"}} amount="5000" desc="Excellent GPU" hasSold={false} img_url="https://i.ibb.co/x38n99S/Space-Flame-nasa-1100.jpg" alt="anything" >
+
+            </Post>
+            <div>
+            <Post post_id="1881" seller={{id:"1600", name:"Stack"}} amount="6000" desc="Best Deal: Brand New AMD AMD6 CPU, new tech optimized with DDR7 RAM, Overclockable" img_url="https://i.ibb.co/nb3JFNy/CPU-AMD-RYZEN5-basic01.png" postsTitle="AMD5 CPU next Gen AMD6" ></Post>
         <Button />
         <Button value="Testing"></Button>
         <Upload></Upload>
         <input type="text"></input>
         <input type="radio"></input>
+        </div>
         </div>
         </div>
     )
