2e08df3434ffe367169fa6662cd434b6cf01a4b6
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2023-07-02 01:58:23 +0800

    adding cros (wip)

--- server/server.js
+++ server/server.js
@@ -5,17 +5,18 @@ const cors = require('cors');
 
 function setupServer() {
     const app = express();
+    const corsOptions = {
+        origin: 'http://localhost:3000',
+        optionsSuccessStatus: 200,
+    };
+    const middleware = cors(corsOptions);
+    // const middleware = cors();
 
     app.use(express.json());
     app.use(express.static(path.join(__dirname, 'public')));
 
-    const corsOptions = {
-        origin: 'http://localhost:3000',
-        optionsSuccessStatus: 200,
-    }
-
-    app.use(cors(corsOptions));
-    
+    app.use(express.urlencoded({ extended: false }));
+    app.use(middleware);
 
     app.get('/hello', (req, res) => {
         res.send('🌏world');
